53a3ed0be9b69862257cebc6bbb4fb87aa6f531a
Yanfiq <ryanfikri5@gmail.com>
2022-12-20 23:09:00 +0700

    new file:   .vscode/configurationCache.log 	new file:   .vscode/dryrun.log 	new file:   .vscode/targets.log 	modified:   L0122114_Ryan/ArrayOfStruct.exe 	new file:   NIK peserta 	modified:   tempCodeRunnerFile.c 	new file:   tempCodeRunnerFile.exe 	modified:   test.c 	modified:   test.exe

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,2 +1,5 @@
-
-        line=0;
+#include<stdio.h>
+int true =1;
+void test(){printf("string1\t");}
+void test2(){printf("string2\n");}
+int main(void){while(true=1){test();test2();}}
--- test.c
+++ test.c
@@ -1,5 +1,80 @@
 #include<stdio.h>
-int true =1;
-void test(){printf("string1\t");}
-void test2(){printf("string2\n");}
-int main(void){while(true=1){test();test2();}}
+
+int looping();
+
+struct data {
+    char NIK[30];
+    int nomor;
+};
+
+
+int main() {
+    FILE *e;
+    int d;
+    struct data j;
+
+    while ((d=looping()) !=4) {
+        switch(d) {
+            case 1:
+                printf("masukkan NIK peserta:");
+            scanf("%s", j.NIK);
+            e=fopen("NIK peserta", "w");
+
+            if (e !=NULL) {
+                fprintf(e, "%s\n", j.NIK);
+                fclose(e);
+                printf("sukses");
+            }
+
+            break;
+
+            case 2: 
+            if ((e=fopen("NIK peserta", "r"))==NULL) {
+                printf("Error: File tidak ada!");
+            }
+
+            while(fgets(j.NIK, sizeof(j.NIK), e)) {
+                printf("%s\n", j.NIK);
+            }
+
+            fclose(e);
+            break;
+
+            case 3: if((e=fopen("NIK peserta", "rb+"))==NULL) {
+                puts("file tidak ditemukan");
+            }
+
+            else {
+                struct data h;
+                printf("masukkan nomor file terakhir:");
+                scanf("%d", &h.nomor);
+
+                    printf("masukkan NIK peserta:");
+                    fscanf(stdin, "%d", &h.NIK);
+                    fseek(e, (h.nomor - 1)*sizeof(struct data), SEEK_SET);
+                    fwrite(&h, sizeof(struct data), 1, e);
+
+                fclose(e);
+            }
+
+            break;
+            
+            default:
+            printf("tidak sesuai petunjuk yang tersedia");
+            break;
+
+
+        }
+    }
+}
+
+int looping() {
+    int menu=0;
+    printf("\n masukkan pilihan\n"
+        "1-membuat file kembali\n"
+        "2-membaca file file kembali\n"
+        "3-menambah file kembali\n"
+        "4-mengakhiri program\n");
+    scanf("%d", &menu);
+    return menu;
+}
